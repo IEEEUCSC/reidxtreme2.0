048e593004d405230bb9463f5c0ca97fe9426ae0
Nipun Chamikara <theharmlesscreeper2002@gmail.com>
2021-09-24 22:28:53 +0530

    Updated timeline component

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -13,66 +13,38 @@ import { GrLaunch as Launch } from "react-icons/gr";
 import "react-vertical-timeline-component/style.min.css";
 import "./styles.css";
 
+// List of events
+const timelineList = [
+  { date: "September 26", icon: <Star />, title: "Launch Event" },
+  { date: "October 3", icon: <Workshop />, title: "Workshop I" },
+  { date: "October 9", icon: <Workshop />, title: "Workshop II" },
+  { date: "October 16 & 17", icon: <Launch />, title: "ReidXtreme" },
+];
+
 const Timeline = () => {
+  // Timeline element
+  const timelinElement = timelineList.map((item, index) => (
+    <VerticalTimelineElement
+      key={index}
+      contentStyle={{
+        background: "rgb(46, 46, 46)",
+        color: "var(--green)",
+      }}
+      contentArrowStyle={{ borderRight: "7px solid  rgb(46, 46, 46)" }}
+      date={item.date}
+      dateClassName="date-light"
+      iconStyle={{ background: "var(--green)", color: "#fff" }}
+      icon={item.icon}
+    >
+      <h3 className="vertical-timeline-element-title">{item.title}</h3>
+    </VerticalTimelineElement>
+  ));
+
   return (
     <section id="timeline" className="timeline">
       <Container>
         <h1 className="section-title">TIMELINE</h1>
-
-        <VerticalTimeline>
-          <VerticalTimelineElement
-            contentStyle={{
-              background: "rgb(46, 46, 46)",
-              color: "var(--green)",
-            }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(46, 46, 46)" }}
-            date="September 26th"
-            dateClassName="date-light"
-            iconStyle={{ background: "var(--green)", color: "#fff" }}
-            icon={<Star />}
-          >
-            <h3 className="vertical-timeline-element-title">Launch Event</h3>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            contentStyle={{
-              background: "rgb(46, 46, 46)",
-              color: "var(--green)",
-            }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(46, 46, 46)" }}
-            date="October 3"
-            dateClassName="date-light"
-            iconStyle={{ background: "var(--green)", color: "#fff" }}
-            icon={<Workshop />}
-          >
-            <h3 className="vertical-timeline-element-title">Workshop I</h3>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            contentStyle={{
-              background: "rgb(46, 46, 46)",
-              color: "var(--green)",
-            }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(46, 46, 46)" }}
-            date="October 9"
-            dateClassName="date-light"
-            iconStyle={{ background: "var(--green)", color: "#fff" }}
-            icon={<Workshop />}
-          >
-            <h3 className="vertical-timeline-element-title">Workshop II</h3>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            contentStyle={{
-              background: "rgb(46, 46, 46)",
-              color: "var(--green)",
-            }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(46, 46, 46)" }}
-            date="October 16 & 17"
-            dateClassName="date-light"
-            iconStyle={{ background: "var(--green)", color: "#fff" }}
-            icon={<Launch />}
-          >
-            <h3 className="vertical-timeline-element-title">ReidXtreme</h3>
-          </VerticalTimelineElement>
-        </VerticalTimeline>
+        <VerticalTimeline>{timelinElement}</VerticalTimeline>
       </Container>
     </section>
   );
